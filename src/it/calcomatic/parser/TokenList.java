2a6af08919421035ec0c11661adea84b215d9554
Emiliano Bovetti <emilianobovetti@hotmail.it>
2014-05-14 12:56:01 +0200

    indent

--- src/it/calcomatic/parser/TokenList.java
+++ src/it/calcomatic/parser/TokenList.java
@@ -6,84 +6,84 @@ import java.util.ListIterator;
 import it.calcomatic.math.Symbol;
 
 public class TokenList extends LinkedList<Symbol> {
-	
+
 	private static final long serialVersionUID = -4206839435081734780L;
-	
+
 	public TokenListIterator tokenIterator() {
-        return this.tokenIterator(0);
-    }
-	
-    public TokenListIterator tokenIterator(int index) {
-        return new TokenListIterator(index);
-    }
-    
-    private class TokenListIterator implements TokenIterator {
-    	
-    	private ListIterator<Symbol> currentIterator;
-    	
-    	private ListIterator<Symbol> lookaheadIterator = null;
-    	
-    	private Symbol lookahead = null;
-    	
-    	TokenListIterator(int index) throws IndexOutOfBoundsException {
-    		if (index < 0 || index > size()) {
-    			throw new IndexOutOfBoundsException();
-    		}
-    		this.initCurrentIterator(index);
-    		this.nextLookahead();
-        }
-    	
-    	private void newCurrentIterator() {
-    		this.initCurrentIterator(this.nextIndex());
-    	}
-    	
-    	private void initCurrentIterator(int index) {
-    		this.currentIterator = listIterator(index);
-    	}
-    	
-    	private boolean newLookaheadIterator() {
-    		return this.initLookaheadIterator(this.nextLookaheadIndex());
-    	}
-    	
-    	private boolean initLookaheadIterator(int index) {
-    		try {
-    			this.lookaheadIterator = listIterator(index);
-    		} catch (IndexOutOfBoundsException e) {
-    			this.lookaheadIterator = null;
-    			return false;
-    		}
-    		return true;
-    	}
-    	
-    	private void nextLookahead() {
-    		if (this.lookaheadIterator == null) {
-    			if (this.initLookaheadIterator(this.nextIndex()))
-    				this.nextLookahead();
-    		} else if (this.lookaheadIterator.hasNext()) {
-    			this.lookahead = this.lookaheadIterator.next();
-    		} else {
-    			this.lookahead = null;
-    			this.lookaheadIterator = null;
-    		}
-    	}
-    	
-    	private void previousLookahead() {
-    		if (this.lookaheadIterator == null) {
-    			if (this.initLookaheadIterator(this.nextIndex()))
-    				this.nextLookahead();
-    		} else if (this.lookaheadIterator.hasPrevious()) {
-    			this.lookahead = this.lookaheadIterator.previous();
-    		} else {
-    			this.lookahead = null;
-    			this.lookaheadIterator = null;
-    		}
-    	}
-    	
-    	@Override
-    	public Symbol getLookahead() {
-    		return this.lookahead;
-    	}
-    	
+		return this.tokenIterator(0);
+	}
+
+	public TokenListIterator tokenIterator(int index) {
+		return new TokenListIterator(index);
+	}
+
+	private class TokenListIterator implements TokenIterator {
+
+		private ListIterator<Symbol> currentIterator;
+
+		private ListIterator<Symbol> lookaheadIterator = null;
+
+		private Symbol lookahead = null;
+
+		TokenListIterator(int index) throws IndexOutOfBoundsException {
+			if (index < 0 || index > size()) {
+				throw new IndexOutOfBoundsException();
+			}
+			this.initCurrentIterator(index);
+			this.nextLookahead();
+		}
+
+		private void newCurrentIterator() {
+			this.initCurrentIterator(this.nextIndex());
+		}
+
+		private void initCurrentIterator(int index) {
+			this.currentIterator = listIterator(index);
+		}
+
+		private boolean newLookaheadIterator() {
+			return this.initLookaheadIterator(this.nextLookaheadIndex());
+		}
+
+		private boolean initLookaheadIterator(int index) {
+			try {
+				this.lookaheadIterator = listIterator(index);
+			} catch (IndexOutOfBoundsException e) {
+				this.lookaheadIterator = null;
+				return false;
+			}
+			return true;
+		}
+
+		private void nextLookahead() {
+			if (this.lookaheadIterator == null) {
+				if (this.initLookaheadIterator(this.nextIndex()))
+					this.nextLookahead();
+			} else if (this.lookaheadIterator.hasNext()) {
+				this.lookahead = this.lookaheadIterator.next();
+			} else {
+				this.lookahead = null;
+				this.lookaheadIterator = null;
+			}
+		}
+
+		private void previousLookahead() {
+			if (this.lookaheadIterator == null) {
+				if (this.initLookaheadIterator(this.nextIndex()))
+					this.nextLookahead();
+			} else if (this.lookaheadIterator.hasPrevious()) {
+				this.lookahead = this.lookaheadIterator.previous();
+			} else {
+				this.lookahead = null;
+				this.lookaheadIterator = null;
+			}
+		}
+
+		@Override
+		public Symbol getLookahead() {
+			return this.lookahead;
+		}
+
 		@Override
 		public boolean hasNext() {
 			return this.lookahead != null;
@@ -115,7 +115,7 @@ public class TokenList extends LinkedList<Symbol> {
 		public int previousIndex() {
 			return this.currentIterator.previousIndex();
 		}
-		
+
 		@Override
 		public int nextLookaheadIndex() {
 			return this.lookaheadIterator.nextIndex();
@@ -125,7 +125,7 @@ public class TokenList extends LinkedList<Symbol> {
 		public int previousLookaheadIndex() {
 			return this.lookaheadIterator.previousIndex();
 		}
-		
+
 		@Override
 		public void remove() {
 			this.currentIterator.remove();
@@ -143,23 +143,23 @@ public class TokenList extends LinkedList<Symbol> {
 			this.currentIterator.add(e);
 			this.newLookaheadIterator();
 		}
-		
+
 		@Override
 		public void removeLookahead() {
 			this.lookaheadIterator.remove();
 			this.newCurrentIterator();
 		}
-		
+
 		@Override
 		public void setLookahead(Symbol e) {
 			this.lookaheadIterator.set(e);
 			this.newCurrentIterator();
 		}
-		
+
 		@Override
 		public void addLookahead(Symbol e) {
 			this.lookaheadIterator.add(e);
 			this.newCurrentIterator();
 		}
-    }
+	}
 }
